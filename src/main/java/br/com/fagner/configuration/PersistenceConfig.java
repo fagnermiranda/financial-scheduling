b00922c06945b104d033b35f106fc24037e78e58
Fagner Miranda Caldas <fagner.miranda01@gmail.com>
2021-11-10 19:53:52 -0300

    Classe de configuração PersistenceConfig para o JPA e o H2

---
+++ src/main/java/br/com/fagner/configuration/PersistenceConfig.java
@@ -0,0 +1,9 @@
+package br.com.fagner.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "br.com.fagner.repository") 
+public class PersistenceConfig { 
+}
