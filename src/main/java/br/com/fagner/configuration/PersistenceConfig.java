52c9f401154649130154d39a4cd84874e37515ff
Fagner Miranda Caldas <fagner.miranda01@gmail.com>
2021-11-13 14:07:04 -0300

    Renomeando para PersistenceConfiguration

--- src/main/java/br/com/fagner/configuration/PersistenceConfig.java
+++
@@ -1,9 +0,0 @@
-package br.com.fagner.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = "br.com.fagner.repository") 
-public class PersistenceConfig { 
-}
